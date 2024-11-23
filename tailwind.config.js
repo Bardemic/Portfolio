69f9243c2181900eb921a45f9028cb7a1f365781
Bardemic <134565367+Bardemic@users.noreply.github.com>
2024-11-23 03:16:26 -0600

    Vite Config Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 export default {
     content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", 
-  ],
-  theme: {
-    extend: {
-      screens: {
-        mobile: '200px',
-        md: '200px'
-      }
-    }
-  }
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ]
 }
